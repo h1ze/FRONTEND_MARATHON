23ab42dd27e062650106382aedd120b268301bb7
h1ze <burgov@mail.ru>
2022-08-03 19:20:44 +0400

    update app.js

--- 04-board/app.js
+++ 04-board/app.js
@@ -1,23 +1,19 @@
 const board = document.querySelector("#board");
-const colors = ["#e73c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
 const SQUARES_NUMBER = 500;
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
 
-  square.addEventListener("mouseover", () => {
-    setColor(square);
-  });
+  square.addEventListener("mouseover", setColor);
 
-  square.addEventListener("mouseleave", () => {
-    removeColor(square);
-  });
+  square.addEventListener("mouseleave", removeColor);
 
   board.append(square);
 }
 
-function setColor(element) {
+function setColor(event) {
+  const element = event.target;
   const color = getRandomColor();
   element.style.backgroundColor = color;
   element.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`;
@@ -29,6 +25,5 @@ function removeColor(element) {
 }
 
 function getRandomColor() {
-  const index = Math.floor(Math.random() * colors.length);
-  return colors[index];
+  return colors[Math.floor(Math.random() * colors.length)];
 }

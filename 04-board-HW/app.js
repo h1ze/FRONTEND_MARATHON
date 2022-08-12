c30016ebab5a2e4c3cb76c89acea6e7e9ee48099
h1ze <burgov@mail.ru>
2022-08-12 22:57:02 +0400

    create app.js

---
+++ 04-board-HW/app.js
@@ -0,0 +1,43 @@
+const board = document.querySelector("#board");
+const colors = [
+  "#7fffd4",
+  "#ff7f50",
+  "#6495ed",
+  "#9932cc2",
+  "#ffe4e11",
+  "#9acd32",
+  "#b22222",
+  "#ffff00",
+];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => {
+    setColor(square);
+  });
+
+  square.addEventListener("mouseleave", () => {
+    removeColor(square);
+  });
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color},0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
